94f0fdddba3fdb5052d0d08f70dc1b503ad30672
Olfredos6 <Alfred.nehemie@gmail.com>
2015-08-10 01:06:25 +0200

    First commit

---
+++ pinger/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pinger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random ipp = new Random();
+            string ipAddress;
+
+            for (int i = 0; i < 10;i++ )
+            {
+            ipAddress = ipp.Next(255) + "." + ipp.Next(255) + "." + ipp.Next(255) + "." + ipp.Next(255);
+            Process.Start("ping", ipAddress).WaitForExit();
+                }
+        }
+    }
+}
